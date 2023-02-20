4ff7fd3bc6ca3a60c2d7bec2643c704f6c8214f6
Akim4XXX <83224199+Akim4XXX@users.noreply.github.com>
2023-02-20 22:37:03 +0300

    Добавьте файлы проекта.

---
+++ КрестикиНолики/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace КрестикиНолики
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Z3 = new System.Windows.Forms.Button();
+            this.Z2 = new System.Windows.Forms.Button();
+            this.Z1 = new System.Windows.Forms.Button();
+            this.Y1 = new System.Windows.Forms.Button();
+            this.Y2 = new System.Windows.Forms.Button();
+            this.Y3 = new System.Windows.Forms.Button();
+            this.X3 = new System.Windows.Forms.Button();
+            this.X2 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.X1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.Z3);
+            this.panel1.Controls.Add(this.Z2);
+            this.panel1.Controls.Add(this.Z1);
+            this.panel1.Controls.Add(this.Y1);
+            this.panel1.Controls.Add(this.Y2);
+            this.panel1.Controls.Add(this.Y3);
+            this.panel1.Controls.Add(this.X3);
+            this.panel1.Controls.Add(this.X2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.X1);
+            this.panel1.Location = new System.Drawing.Point(20, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(292, 291);
+            this.panel1.TabIndex = 0;
+            // 
+            // Z3
+            // 
+            this.Z3.Location = new System.Drawing.Point(194, 191);
+            this.Z3.Name = "Z3";
+            this.Z3.Size = new System.Drawing.Size(83, 82);
+            this.Z3.TabIndex = 0;
+            this.Z3.UseVisualStyleBackColor = true;
+            this.Z3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // Z2
+            // 
+            this.Z2.Location = new System.Drawing.Point(105, 191);
+            this.Z2.Name = "Z2";
+            this.Z2.Size = new System.Drawing.Size(83, 82);
+            this.Z2.TabIndex = 0;
+            this.Z2.UseVisualStyleBackColor = true;
+            this.Z2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // Z1
+            // 
+            this.Z1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Z1.Location = new System.Drawing.Point(16, 191);
+            this.Z1.Name = "Z1";
+            this.Z1.Size = new System.Drawing.Size(83, 82);
+            this.Z1.TabIndex = 0;
+            this.Z1.UseVisualStyleBackColor = true;
+            this.Z1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // Y1
+            // 
+            this.Y1.Location = new System.Drawing.Point(16, 103);
+            this.Y1.Name = "Y1";
+            this.Y1.Size = new System.Drawing.Size(83, 82);
+            this.Y1.TabIndex = 0;
+            this.Y1.UseVisualStyleBackColor = true;
+            this.Y1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // Y2
+            // 
+            this.Y2.Location = new System.Drawing.Point(105, 103);
+            this.Y2.Name = "Y2";
+            this.Y2.Size = new System.Drawing.Size(83, 82);
+            this.Y2.TabIndex = 0;
+            this.Y2.UseVisualStyleBackColor = true;
+            this.Y2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // Y3
+            // 
+            this.Y3.Location = new System.Drawing.Point(194, 103);
+            this.Y3.Name = "Y3";
+            this.Y3.Size = new System.Drawing.Size(83, 82);
+            this.Y3.TabIndex = 0;
+            this.Y3.UseVisualStyleBackColor = true;
+            this.Y3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // X3
+            // 
+            this.X3.Location = new System.Drawing.Point(194, 15);
+            this.X3.Name = "X3";
+            this.X3.Size = new System.Drawing.Size(83, 82);
+            this.X3.TabIndex = 0;
+            this.X3.UseVisualStyleBackColor = true;
+            this.X3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // X2
+            // 
+            this.X2.Location = new System.Drawing.Point(105, 15);
+            this.X2.Name = "X2";
+            this.X2.Size = new System.Drawing.Size(83, 82);
+            this.X2.TabIndex = 0;
+            this.X2.UseVisualStyleBackColor = true;
+            this.X2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 82);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // X1
+            // 
+            this.X1.Location = new System.Drawing.Point(16, 15);
+            this.X1.Name = "X1";
+            this.X1.Size = new System.Drawing.Size(83, 82);
+            this.X1.TabIndex = 0;
+            this.X1.UseVisualStyleBackColor = true;
+            this.X1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 325);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Крестики Нолики";
+            this.Click += new System.EventHandler(this.buttonClick);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Z3;
+        private System.Windows.Forms.Button Z2;
+        private System.Windows.Forms.Button Z1;
+        private System.Windows.Forms.Button Y1;
+        private System.Windows.Forms.Button Y2;
+        private System.Windows.Forms.Button Y3;
+        private System.Windows.Forms.Button X3;
+        private System.Windows.Forms.Button X2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button X1;
+    }
+}
+
---
+++ КрестикиНолики/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace КрестикиНолики
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true;
+        int turnCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+
+            if (turn)
+            {
+                b.BackColor = Color.Red;
+            }
+            else
+                b.BackColor = Color.Green;
+            turn = !turn;
+            b.Enabled = false;
+            turnCount++;
+            CheckWinner();
+        }
+        private void CheckWinner()
+        {
+            bool win = false;
+            if (X1.BackColor == X2.BackColor & X2.BackColor == X3.BackColor & !X1.Enabled) win = true;
+            if (Y1.BackColor == Y2.BackColor & Y2.BackColor == Y3.BackColor & !Y1.Enabled) win = true;
+            if (Z1.BackColor == Z2.BackColor & Z2.BackColor == Z3.BackColor & !Z1.Enabled) win = true;
+
+            if (X1.BackColor == Y1.BackColor & Y1.BackColor == Z1.BackColor & !X1.Enabled) win = true;
+            if (X2.BackColor == Y2.BackColor & Y2.BackColor == Z2.BackColor & !X2.Enabled) win = true;
+            if (X3.BackColor == Y3.BackColor & Y3.BackColor == Z3.BackColor & !X3.Enabled) win = true;
+
+            if (X1.BackColor == Y2.BackColor & Y2.BackColor == Z3.BackColor & !X1.Enabled) win = true;
+            if (X3.BackColor == Y2.BackColor & Y2.BackColor == Z1.BackColor & !X3.Enabled) win = true;
+
+            if (win)
+            {
+                if (turn)
+                {
+                    MessageBox.Show("Победа - Зелёный");
+                }
+                else
+                {
+                    MessageBox.Show("Победа - Красный");
+                }
+
+            }
+            else if (turnCount == 9)
+            {
+                 MessageBox.Show("Ничья");
+            }
+        }
+    }
+}
